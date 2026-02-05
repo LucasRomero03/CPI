8856ba66e444effd2bbe4d2a954fa2a20732644f
Lucas Romero <lucasromero03@gmail.com>
2026-02-05 18:39:25 -0300

    subindo tenant e scprit da integração com o mercado livre

--- scriptOpenW.groovy
+++ scriptOpenW.groovy
@@ -6,31 +6,20 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 
-Message processData(Message message) {
+Message processData(Message Message) {
     def body = message.getBody(String)
     def jsonSlurper = new JsonSlurper() 
-    def bodyParsed  = jsonSlurper.parseText(body)
-     
+    def input  = jsonSlurper.parseText(body)
+    def cidade = input.cidade 
 
     // Subistitua "SUA_CHAVE" pela sua chave de APi do OpenWeather 
-    def apiKey = "715e722a05416ed98e52a6a8381e73cb"
-    def url;
-
+    def apiKey = "c505249a4f9ff095792fb433c7ad03b0"
+    // Codificar o nome da cidade
+    def cicadeCodificada = URLEncoder.encode(cidade,"UTF-8")
     // Construir a url usando o nome codificado 
+    def url = new URL("https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br")
+    def url = new URL("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
 
-      if(bodyParsed.cidade){
-        // Codificar o nome da cidade, trazer aquele padrão de url Rio+de+janeiro 
-        def cidadeCodificada = URLEncoder.encode(bodyParsed.cidade,"UTF-8")
-        //Construir a url
-        url = new URL("https://api.openweathermap.org/data/2.5/weather?q=${cidadeCodificada}&appid=${apiKey}&units=metric&lang=pt_br")
-
-    }else{
-        def lat = bodyParsed.lat;
-        def lon = bodyParsed.lon;
-        //Construir a url
-        url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}")
-
-    }
    
     // Realizar a chamada HTTP GET
     HttpURLConnection connection = (HttpURLConnection) url.openConnection()
@@ -39,13 +28,19 @@ Message processData(Message message) {
 
     // Verificar o codigo de resposta 
     def responseCode = connection.responseCode
-    def resposta = (responseCode == 200) ? connection.inputStream.text :  connection.errorStream?.text ?: {} ;
-    
-    def output = jsonSlurper.parseText(resposta)
-    // Retornar a resposta formatada com JSON 
-    message.setBody(JsonOutput.toJson(output))
-   
-    message.setHeader("Content-Type","application/json")
+    if (responseCode == 200) {
+        // Ler a resposta da API 
+        def responseStream = connection.inputStream.text
+        def responseJson = new JsonSlurper().parseText(responseStream)
+
+        // Retornar a resposta formatada com JSON 
+        message.setBody(JsonOutput.toJson(responseJson))
+
+    } else {
+        message.setBody(JsonOutput.toJson([error: "Erro ao consultar a API OpenWeather"]))
+
+    }
+
     return message
 
 }
